3d3b7ad2fdfc2ce10c52c0cbfa91cd2c383c0ab7
Ran Pol <rnd809@gmail.com>
2022-10-07 09:32:34 -0400

    Delete the unecessary files produce by Create-React-App. Also, add the locations data for the travel post. Additionaly place the images.

---
+++ src/data/locations.js
@@ -0,0 +1,64 @@
+const locations = [
+  {
+    title: 'Hollywod',
+    location: 'Los Angeles, USA',
+    googleMapsUrl: 'https://goo.gl/maps/wmK2MSSbFzzmmJzm9',
+    startDate: '2014',
+    endDate: '24 Jan, 2017',
+    description:
+      'The Hollywood Sign is an American landmark and cultural icon overlooking Hollywood, Los Angeles, California. It is situated on Mount Lee, in the Beachwood Canyon area of the Santa Monica Mountains.',
+    imageUrl: 'cali.jpg',
+  },
+  {
+    title: 'Niagara Falls',
+    location: 'Ontario, CA',
+    googleMapsUrl: 'https://goo.gl/maps/i7s9poRt5cEzCSr3A',
+    startDate: '2006',
+    endDate: '2006',
+    description:
+      "Niagara Falls, Ontario, is a Canadian city at the famous waterfalls of the same name, linked with the U.S. by the Rainbow Bridge. Its site on the Niagara River's western shore overlooks the Horseshoe Falls, the cascades' most expansive section. Elevators take visitors to a lower, wetter vantage point behind the falls. A cliffside park features a promenade alongside 520-ft.-high Skylon Tower with an observation deck.",
+    imageUrl: 'canada.jpg',
+  },
+  {
+    title: 'U.S. Capital',
+    location: 'Washington, DC',
+    googleMapsUrl: 'https://goo.gl/maps/Mg5BP6gkSGhxrB737',
+    startDate: '2002',
+    endDate: '2020',
+    description:
+      "Washington, DC, the U.S. capital, is a compact city on the Potomac River, bordering the states of Maryland and Virginia. It’s defined by imposing neoclassical monuments and buildings – including the iconic ones that house the federal government’s 3 branches: the Capitol, White House and Supreme Court. It's also home to iconic museums and performing-arts venues such as the Kennedy Center.",
+    imageUrl: 'dc.jpg',
+  },
+  {
+    title: 'Dubai',
+    location: 'Dubai, UAE',
+    googleMapsUrl: 'https://goo.gl/maps/HhRcZ51JfBPk2VDm9',
+    startDate: '1993',
+    endDate: '2020',
+    description:
+      'Dubai is a city and emirate in the United Arab Emirates known for luxury shopping, ultramodern architecture and a lively nightlife scene. Burj Khalifa, an 830m-tall tower, dominates the skyscraper-filled skyline. At its foot lies Dubai Fountain, with jets and lights choreographed to music. On artificial islands just offshore is Atlantis, The Palm, a resort with water and marine-animal parks.',
+    imageUrl: 'dubai.jpg',
+  },
+  {
+    title: 'Boca Chica',
+    location: 'Boca Chica, DR',
+    googleMapsUrl: 'https://goo.gl/maps/wP5AFh1ofUkZvQCT6',
+    startDate: '1993',
+    endDate: '2020',
+    description:
+      "Boca Chica is a Caribbean town in the Dominican Republic, near Santo Domingo city. It's known for Boca Chica Beach, with its white sand and sheltered waters. In the shallow bay, the mangrove-covered islet La Matica is a habitat for birds. Coral reefs and submerged shipwrecks feature at La Caleta Underwater Park, west of town. Calle Duarte, the main street, is pedestrian-only in the evenings and has lively nightlife.",
+    imageUrl: 'dominican.jpg',
+  },
+  {
+    title: 'Tijuana',
+    location: 'Baja California, MX',
+    googleMapsUrl: 'https://goo.gl/maps/A8MrrDJ17R8M37hp7',
+    startDate: '1993',
+    endDate: '2020',
+    description:
+      'Tijuana is a border city in Mexico, just south of California. Its bustling main street, Avenida Revolución, is lined with souvenir shops and lively bars. Landmarks include the neoclassical Jai Alai Frontón palace and Centro Cultural Tijuana, a modern cultural complex in the Zona Río district. Throughout town, stadiums stage lucha libre (wrestling) matches, while the nearby city of Rosarito fronts sandy beaches.',
+    imageUrl: 'mexico.jpg',
+  },
+]
+
+export default locations
